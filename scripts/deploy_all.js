25d9349fe50f52b8324bc8e903ad93a761200b0d
Andrey Khalov <a.khalov@gmail.com>
2025-02-12 09:40:00 +0500

    all contracts deployed

--- scripts/deploy_all.js
+++ scripts/deploy_all.js
@@ -2,53 +2,56 @@
 const hre = require("hardhat");
 
 async function main() {
+  // Retrieve ethers from hre
+  const { ethers } = hre;
+  // In ethers v6, use ethers.parseEther directly
+  const parseEther = ethers.parseEther;
+
   // 1) Deploy MockERC20
-  const MockERC20 = await hre.ethers.getContractFactory("MockERC20");
-  const token = await MockERC20.deploy("EXE Token", "EXE", hre.ethers.utils.parseEther("1000000"));
-  await token.deployed();
-  console.log("MockERC20 deployed at:", token.address);
+  const MockERC20 = await ethers.getContractFactory("MockERC20");
+  const token = await MockERC20.deploy("EXE Token", "EXE", parseEther("1000000"));
+  await token.waitForDeployment();
+  console.log("MockERC20 deployed at:", token.target);
 
   // 2) Deploy EXETaskManager
-  const EXETaskManager = await hre.ethers.getContractFactory("EXETaskManager");
-  const taskManager = await EXETaskManager.deploy(token.address);
-  await taskManager.deployed();
-  console.log("EXETaskManager deployed at:", taskManager.address);
+  const EXETaskManager = await ethers.getContractFactory("EXETaskManager");
+  const taskManager = await EXETaskManager.deploy(token.target);
+  await taskManager.waitForDeployment();
+  console.log("EXETaskManager deployed at:", taskManager.target);
 
   // 3) Deploy AgentController
-  const AgentController = await hre.ethers.getContractFactory("AgentController");
+  const AgentController = await ethers.getContractFactory("AgentController");
   const agentCtrl = await AgentController.deploy();
-  await agentCtrl.deployed();
-  console.log("AgentController deployed at:", agentCtrl.address);
+  await agentCtrl.waitForDeployment();
+  console.log("AgentController deployed at:", agentCtrl.target);
 
   // 4) Deploy GraphSync
-  const GraphSync = await hre.ethers.getContractFactory("GraphSync");
+  const GraphSync = await ethers.getContractFactory("GraphSync");
   const graphSync = await GraphSync.deploy();
-  await graphSync.deployed();
-  console.log("GraphSync deployed at:", graphSync.address);
+  await graphSync.waitForDeployment();
+  console.log("GraphSync deployed at:", graphSync.target);
 
   // 5) Deploy dAppProxyModule
-  const ProxyModule = await hre.ethers.getContractFactory("dAppProxyModule");
+  const ProxyModule = await ethers.getContractFactory("dAppProxyModule");
   const proxy = await ProxyModule.deploy();
-  await proxy.deployed();
-  console.log("dAppProxyModule deployed at:", proxy.address);
+  await proxy.waitForDeployment();
+  console.log("dAppProxyModule deployed at:", proxy.target);
 
   // 6) Deploy TokenBurner
-  const TokenBurner = await hre.ethers.getContractFactory("TokenBurner");
+  const TokenBurner = await ethers.getContractFactory("TokenBurner");
   const burner = await TokenBurner.deploy();
-  await burner.deployed();
-  console.log("TokenBurner deployed at:", burner.address);
+  await burner.waitForDeployment();
+  console.log("TokenBurner deployed at:", burner.target);
 
   // 7) Deploy ZKValidator
-  const ZKValidator = await hre.ethers.getContractFactory("ZKValidator");
+  const ZKValidator = await ethers.getContractFactory("ZKValidator");
   const zk = await ZKValidator.deploy();
-  await zk.deployed();
-  console.log("ZKValidator deployed at:", zk.address);
+  await zk.waitForDeployment();
+  console.log("ZKValidator deployed at:", zk.target);
 
   console.log("\nAll contracts deployed successfully!");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
